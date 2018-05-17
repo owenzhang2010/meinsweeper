982ce71a89da0446c0810898b84dc8139c12f686
owenzhang2010 <owenzhang2010@berkeley.edu>
2018-05-17 10:57:15 -0400

    jsons and stuff

---
+++ src/StatsHelper.java
@@ -0,0 +1,28 @@
+public class StatsHelper {
+    public static String getDifficulty() {
+        return null;
+    }
+
+    public static void setDifficulty() {
+
+    }
+
+    public static void winGame() { // update all stats for gamemode
+
+    }
+
+    public static void loseGame() { // update played for gamemode
+
+    }
+
+    public static void resetMode(String mode) {
+
+    }
+
+    public static void resetAll() {
+        resetMode("beginner");
+        resetMode("intermediate");
+        resetMode("expert");
+        resetMode("lottery");
+    }
+}
