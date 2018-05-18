173e125a27db230a28aff553bdf43809de37b689
owenzhang2010 <owenzhang2010@berkeley.edu>
2018-05-18 17:42:47 -0400

    statsbox display

--- src/StatsBox.java
+++ src/StatsBox.java
@@ -1,7 +1,31 @@
 import java.util.Map;
 
-public class StatsBox {
-    public static void display(String difficulty, Map<String, Object> stats) {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.stage.Modality;
+import javafx.scene.text.Text;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 
+
+public class StatsBox {
+    public static void display(String difficulty, Map<String, String> stats) {
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(difficulty.substring(0, 1).toUpperCase() + difficulty.substring(1) + " Stats");
+
+        String text = "Average time: " + stats.get("average") + "\n"
+                    + "Best time: " + stats.get("best") + "\n"
+                    + "Wins: " + stats.get("wins") + "\n"
+                    + "Gamed played: " + stats.get("played") + "\n"
+                    + "Win percentage: " + stats.get("percentage") + "\n";
+        Text t = new Text(text);
+
+        GridPane g = new GridPane();
+        g.getChildren().add(t);
+        Scene scene = new Scene(g, 150, 150);
+        window.setScene(scene);
+        window.show();
     }
 }
--- src/StatsHelper.java
+++ src/StatsHelper.java
@@ -1,10 +1,10 @@
 import java.io.File;
+import java.util.Map;
+import java.util.HashMap;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
@@ -13,36 +13,37 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 public class StatsHelper {
+    private static Document document;
+
     public static void xmlSetup() {
-        if (!(new File("stats.xml").exists())) {
+        File f = new File("stats.xml");
+        if (!f.exists()) {
             try {
                 DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
                 DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-                Document doc = docBuilder.newDocument();
+                document = docBuilder.newDocument();
 
-                Element rootElement = doc.createElement("stats");
-                doc.appendChild(rootElement);
+                Element rootElement = document.createElement("stats");
+                document.appendChild(rootElement);
 
-                initializeStatsHelper(doc, rootElement, "beginner");
-                initializeStatsHelper(doc, rootElement, "intermediate");
-                initializeStatsHelper(doc, rootElement, "expert");
-                initializeStatsHelper(doc, rootElement, "lottery");
+                initializeStatsHelper(rootElement, "beginner");
+                initializeStatsHelper(rootElement, "intermediate");
+                initializeStatsHelper(rootElement, "expert");
+                initializeStatsHelper(rootElement, "lottery");
 
                 TransformerFactory transformerFactory = TransformerFactory.newInstance();
                 Transformer transformer = transformerFactory.newTransformer();
-                DOMSource source = new DOMSource(doc);
+                DOMSource source = new DOMSource(document);
                 StreamResult result = new StreamResult(new File("stats.xml"));
                 transformer.transform(source, result);
-            } catch (ParserConfigurationException pce) {
-                pce.printStackTrace();
-            } catch (TransformerException tfe) {
-                tfe.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
 
-    private static void initializeStatsHelper(Document d, Element root, String difficulty) {
-        Element e = d.createElement(difficulty);
+    private static void initializeStatsHelper(Element root, String difficulty) {
+        Element e = document.createElement(difficulty);
         e.setAttribute("average", "NaN");
         e.setAttribute("best", "NaN");
         e.setAttribute("wins", "0");
@@ -52,14 +53,27 @@ public class StatsHelper {
     }
 
     public static void showStats(String difficulty) {
-        
+        Map<String, String> stats = new HashMap<>();
+        File f = new File("stats.xml");
+        try {
+            document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(f);
+            Element e = (Element) document.getElementsByTagName(difficulty).item(0);
+            stats.put("average", e.getAttribute("average"));
+            stats.put("best", e.getAttribute("best"));
+            stats.put("wins", e.getAttribute("wins"));
+            stats.put("played", e.getAttribute("played"));
+            stats.put("percentage: ", e.getAttribute("percentage"));
+            StatsBox.display(difficulty, stats);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void winGame() { // update all stats for gamemode
+    public static void winGame() { // update all stats for game mode
 
     }
 
-    public static void loseGame() { // update played/% for gamemode
+    public static void loseGame() { // update played/% for game mode
 
     }
 
